a84729c9778c666393974aa708e31a782cb39bcc
gelson João <gelsonjupiter@gmail.com>
2020-03-17 12:31:14 +0100

    configurando o gulp tasks no projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,26 @@
 var gulp = require('gulp');
-
-gulp.task('bacon', function () {
-   console.log('Whow, bacon is so juicy!');
+var sass = require('gulp-sass');
+var uglifycss = require('gulp-uglifycss');
+ 
+gulp.task('sass', function () {
+   return gulp.src('webapp/scss/*.scss')
+   .pipe(sass().on('error', sass.logError))
+   .pipe(gulp.dest('./webapp/css'))
 });
+gulp.task('css', function () {
+   return gulp.src('./styles/**/*.css')
+      .pipe(uglifycss({
+        "maxLineLen": 80,
+        "uglyComments": true
+      }))
+      .pipe(gulp.dest('./dist/'));
+  });
+
+  gulp.task('run', ['sass', 'css']);
+
+  gulp.task('watch', function () {
+      gulp.watch('./sass/*sass', ['sass']);
+      gulp.watch('./css/*.css', []);
+  });
+
+  gulp.task('default', ['run', 'watch']);
